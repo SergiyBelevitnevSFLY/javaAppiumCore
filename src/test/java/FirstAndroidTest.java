4a117316e876034db935e71bba282799a6e11ba0
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-10-04 12:11:28 +0200

    Klimenko: Appium task fix2

--- src/test/java/FirstAndroidTest.java
+++ src/test/java/FirstAndroidTest.java
@@ -6,29 +6,30 @@ public class FirstAndroidTest extends BaseAndroidTest {
     @Test
     public void firstAndroidTest() {
         SwagLoginPage swagLoginPage = new SwagLoginPage(getDriver());
+        SwagProductPage swagProductPage = new SwagProductPage(getDriver());
+        SwagMenuPage swagMenuPage = new SwagMenuPage(getDriver());
+        SwagFilterPage swagFilterPage = new SwagFilterPage(getDriver());
+        SwagCartPage swagCartPage = new SwagCartPage(getDriver());
+        SwagCheckoutPage swagCheckoutPage = new SwagCheckoutPage(getDriver());
+
         swagLoginPage.login("standard_user", "secret_sauce");
 
-        SwagProductPage swagProductPage = new SwagProductPage(getDriver());
         swagProductPage.clickMenuButton();
 
-        SwagMenuPage swagMenuPage = new SwagMenuPage(getDriver());
         swagMenuPage.clickAllItemsOption();
+
         swagMenuPage.clickCloseButton();
 
         swagProductPage.clickFilterButton();
 
-        SwagFilterPage swagFilterPage = new SwagFilterPage(getDriver());
         swagFilterPage.clickNameZtoA();
 
         swagProductPage.clickAddToCartFirstElement();
 
         swagProductPage.clickCartButton();
 
-        SwagCartPage swagCartPage = new SwagCartPage(getDriver());
-
         swagCartPage.clickCheckoutButton();
 
-        SwagCheckoutPage swagCheckoutPage = new SwagCheckoutPage(getDriver());
         swagCheckoutPage.continueCheckout("First Name", "Last Name", "11250");
 
     }
